48c452ea71d6bbb293b294b94d8ec9d6edb8e8a2
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2023-01-27 16:51:11 -0300

    1.1.1.27.23 - Segunda Versão

--- Aula 2/app.js
+++ Aula 2/app.js
@@ -2,11 +2,12 @@
  * Objetivo: Calcular a média de 4 notas escolares. 
  * Autor: Lohannes da Silva Costa
  * Data: 27/01/2023
- * Versão: 1.0.1.27.23
+ * Versão: 1.1.1.27.23
  ********************************************************************************************************************/
 
 //Import da Biblioteca ReadLine.
 var readLine = require('readline');
+const { isNull } = require('util');
 
 //Cria o objeto para usar o  especialista em entrada de dados pelo teclado.
 var entradaDados = readLine.createInterface({
@@ -48,16 +49,26 @@ entradaDados.question('Digite seu nome: \n', function (nome) {
                 //Função de Callback para entrar a quarta nota.
                 entradaDados.question('Digite a terceira nota: \n', function (nota4) {
                     let quartaNota = nota4;
-                    let media = (parseFloat(primeiraNota) + parseFloat(segundaNota) + parseFloat(terceiraNota) + parseFloat(quartaNota)) / 4;
 
-                    console.log('Nome do Aluno: ' + nome + '\nPortuguês: ' + nota1 + '\nMatemática: '
-                    + nota2 + '\nInglês: ' + nota3 + '\nEd. Física: ' + nota4 + '\n-------------' + '\nMédia Final: ' + media);
-                    
-                    if (media < 5){
-                        console.log('\nAluno Reprovado');
+                    if ((primeiraNota || segundaNota || terceiraNota || quartaNota == '')) {
+                        console.log('\nERRO: Algum dado não foi fornecido'); 
+                    } else if (isNaN(primeiraNota) || isNaN(segundaNota) || isNaN(terceiraNota) || isNaN(quartaNota)) {
+                        console.log('\nERRO: Alguma das notas não é um Número'); 
+                    } else if (((primeiraNota || segundaNota || terceiraNota || quartaNota) > 10)){
+                        console.log('\nERRO: Alguma das notas ultrapassou o limite');
                     } else {
-                        console.log('\nAluno Aprovado');
+                        let media = (parseFloat(primeiraNota) + parseFloat(segundaNota) + parseFloat(terceiraNota) + parseFloat(quartaNota)) / 4;
+
+                        console.log('Nome do Aluno: ' + nome + '\nPortuguês: ' + nota1 + '\nMatemática: '
+                        + nota2 + '\nInglês: ' + nota3 + '\nEd. Física: ' + nota4 + '\n-------------' + '\nMédia Final: ' + media);
+                    
+                        if (media < 5){
+                            console.log('\nAluno Reprovado');
+                        } else {
+                            console.log('\nAluno Aprovado');
+                        }
                     }
+                    
 
                 });
 
