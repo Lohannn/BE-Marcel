2f46d477bcf19e86cf2567747d1e014d8d77f346
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2023-04-14 16:26:56 -0300

    iniciando

--- Aula 7/app.js
+++ Aula 7/app.js
@@ -14,9 +14,47 @@ const app = express()
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*')
 
-    response.header('Access-Control-Allow-Methods', 'GEt, POST, PUT, DELETE, OPTIONS')
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 
     app.use(cors())
 
     next()
+})
+
+//CRUD (Create, Read, Update, Delete)
+
+/*************************************************************************************
+ * Objetibo: API de controle de Alunos.
+ * Data: 14/04/2023
+ * Autor: Lohannes da Silva Costa
+ * Versão: 1.0
+ *************************************************************************************/
+
+    //Endpoint: Retorna todos os dados de alunos
+    app.get('/v1/lion-school/aluno', cors(), async, function (request, response){
+
+    })
+
+    //Endpoint: Retorna o aluno filtrando pelo ID
+    app.get('/v1/lion-school/aluno/:id', cors(), async, function (request, response){
+        
+    })
+
+    //Endpoint: Insere um dado novo
+    app.post('/v1/lion-school/aluno', cors(), async, function (request, response){
+        
+    })
+
+    //Endpoint: Atualiza um dado existente, filtrando pelo ID
+    app.put('/v1/lion-school/aluno/:id', cors(), async, function (request, response){
+        
+    })
+
+    //Endpoint: Deleta um dado existente, filtrando pelo ID
+    app.delete('/v1/lion-school/aluno/:id', cors(), async, function (request, response){
+        
+    })
+
+app.listen(8080, function(){
+    console.log('aguardadno requisições na porta 8080...');
 })
